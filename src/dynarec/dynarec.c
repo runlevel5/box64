runlevel5/box64

commit ad90eaef8dfd22ba43300242564704f25980eab4

Introducing EmuRun, to have proper handling of LongJump on CallBack and on Interpreter only build

src/dynarec/dynarec.c
@@ -118,7 +118,8 @@ void DynaCall(x64emu_t* emu, uintptr_t addr)
         PushExit(emu);
     R_RIP = addr;
     emu->df = d_none;
-    DynaRun(emu);
+    emu->flags.need_jmpbuf = 1;
+    EmuRun(emu, 1);
     emu->quit = 0;  // reset Quit flags...
     emu->df = d_none;
     if(emu->flags.quitonlongjmp && emu->flags.longjmp) {
@@ -142,7 +143,7 @@ void DynaCall(x64emu_t* emu, uintptr_t addr)
 }
 
 extern int running32bits;
-void DynaRun(x64emu_t* emu)
+void EmuRun(x64emu_t* emu, int use_dynarec)
 {
     // prepare setjump for signal handling
     JUMPBUFF jmpbuf[1] = {0};
@@ -166,7 +167,7 @@ void DynaRun(x64emu_t* emu)
             if ((skip = SigSetJmp(emu->jmpbuf, 1)))
             #endif
             {
-                dynarec_log(LOG_DEBUG, "Setjmp DynaRun, fs=0x%x will %sskip dynarec next\n", emu->segs[_FS], (skip==3)?"not ":"");
+                dynarec_log(LOG_DEBUG, "Setjmp EmuRun, fs=0x%x will %sskip dynarec next\n", emu->segs[_FS], (skip==3)?"not ":"");
                 #ifdef DYNAREC
                 if(BOX64ENV(dynarec_test)) {
                     if(emu->test.clean)
@@ -182,7 +183,7 @@ void DynaRun(x64emu_t* emu)
             emu->flags.need_jmpbuf = 0;
 
 #ifdef DYNAREC
-        if(!BOX64ENV(dynarec))
+        if(!BOX64ENV(dynarec) || !use_dynarec)
 #endif
             Run(emu, 0);
 #ifdef DYNAREC
@@ -241,4 +242,9 @@ void DynaRun(x64emu_t* emu)
     #ifdef RV64
     emu->xSPSave = old_savesp;
     #endif
+}
+
+void DynaRun(x64emu_t *emu)
+{
+    EmuRun(emu, 1);
 }

src/emu/x64emu.c
@@ -574,7 +574,8 @@ void EmuCall(x64emu_t* emu, uintptr_t addr)
         PushExit(emu);
     R_RIP = addr;
     emu->df = d_none;
-    Run(emu, 0);
+    emu->flags.need_jmpbuf = 1;
+    EmuRun(emu, 0);
     emu->quit = 0;  // reset Quit flags...
     emu->df = d_none;
     if(emu->flags.quitonlongjmp && emu->flags.longjmp) {

src/include/box64cpu.h
@@ -4,6 +4,7 @@
 typedef struct x64emu_s x64emu_t;
 
 int Run(x64emu_t *emu, int step);
+void EmuRun(x64emu_t* emu, int use_dynarec);
 void EmuCall(x64emu_t* emu, uintptr_t addr);
 void StopEmu(x64emu_t* emu, const char* reason, int is32bits);
 void DynaRun(x64emu_t *emu);

commit 58d3525cbcf28e28e0ef089dcce3c7a9afedc7ad

Fixed a speed regression introduced with ad90eaef8dfd22ba43300242564704f25980eab4 (should help #3573)

src/dynarec/dynarec.c
@@ -133,7 +133,8 @@ void DynaCall(x64emu_t* emu, uintptr_t addr)
         PushExit(emu);
     R_RIP = addr;
     emu->df = d_none;
-    emu->flags.need_jmpbuf = 1;
+    if(emu->flags.quitonlongjmp)
+        emu->flags.need_jmpbuf = 1;
     EmuRun(emu, 1);
     emu->quit = 0;  // reset Quit flags...
     emu->df = d_none;
@@ -166,7 +167,6 @@ void EmuRun(x64emu_t* emu, int use_dynarec)
     #ifdef RV64
     uintptr_t old_savesp = emu->xSPSave;
     #endif
-    emu->flags.jmpbuf_ready = 0;
     int is32bits = (emu->segs[_CS]==0x23);
     while(!(emu->quit)) {
         if(!emu->jmpbuf || (emu->flags.need_jmpbuf && emu->jmpbuf!=jmpbuf)) {

src/emu/x64emu.c
@@ -88,6 +88,8 @@ static void internalX64Setup(x64emu_t* emu, box64context_t *context, uintptr_t s
     // setup fpu regs
     reset_fpu(emu);
     emu->mxcsr.x32 = 0x1f80;
+    // want some new jmpbuf for error recovery
+    emu->flags.need_jmpbuf = 1;
 }
 
 EXPORTDYN
@@ -578,7 +580,8 @@ void EmuCall(x64emu_t* emu, uintptr_t addr)
         PushExit(emu);
     R_RIP = addr;
     emu->df = d_none;
-    emu->flags.need_jmpbuf = 1;
+    if(emu->flags.quitonlongjmp)
+        emu->flags.need_jmpbuf = 1;
     EmuRun(emu, 0);
     emu->quit = 0;  // reset Quit flags...
     emu->df = d_none;