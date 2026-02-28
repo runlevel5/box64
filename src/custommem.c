runlevel5/box64

commit b07e2ea9d7a319df7d61ddf21e21a43b86b72b78

[DYNAREC] Added Secondary Entry Point to dynablock at CALLRET return

src/custommem.c
@@ -1480,6 +1480,7 @@ int MmaplistAddBlock(mmaplist_t* list, int fd, off_t offset, void* orig, size_t 
             GO(instsize);
             GO(arch);
             GO(callrets);
+            GO(sep);
             GO(jmpnext);
             GO(table64);
             GO(relocs);
@@ -1506,6 +1507,14 @@ int MmaplistAddBlock(mmaplist_t* list, int fd, off_t offset, void* orig, size_t 
                 // cannot add blocks?
                 printf_log(LOG_INFO, "Warning, cannot add DynaCache Block %d to JmpTable\n", i);
             } else {
+                for(int i=0; i<bl->sep_size; ++i) {
+                    uint32_t x64_offs = bl->sep[i].x64_offs;
+                    uint32_t nat_offs = bl->sep[i].nat_offs;
+                    if(addJumpTableIfDefault64(bl->x64_addr+x64_offs, (bl->dirty || bl->always_test)?bl->jmpnext:(bl->block+nat_offs)))
+                        bl->sep[i].active = 1;
+                    else
+                        bl->sep[i].active = 0;
+                }
                 if(bl->x64_size) {
                     dynarec_log(LOG_DEBUG, "Added DynCache bl %p for %p - %p\n", bl, bl->x64_addr, bl->x64_addr+bl->x64_size);
                     if(bl->x64_size>my_context->max_db_size) {

src/dynarec/arm64/dynarec_arm64_pass2.h
@@ -30,5 +30,8 @@
                 Table64(dyn, getConst(V), 2); EMIT(0);                          \
         } while(0)
 #define FTABLE64(A, V)  do { mmx87_regs_t v = {.d = V}; Table64(dyn, v.q, 2); EMIT(0); } while(0)
-#define CALLRET_RET()   do { if(BOX64DRENV(dynarec_callret)>1) {dyn->callrets[dyn->callret_size].type = 0; dyn->callrets[dyn->callret_size++].offs = dyn->native_size; EMIT(ARCH_NOP);} } while(0)
+#define CALLRET_RET(A)   do {                                                                                                                                                    \
+                if((A) && BOX64DRENV(dynarec_callret)) { dyn->insts[ninst].size+=sizeof(void*); dyn->native_size+=sizeof(void*); ++dyn->sep_size; }                              \
+                if(BOX64DRENV(dynarec_callret)>1) {dyn->callrets[dyn->callret_size].type = 0; dyn->callrets[dyn->callret_size++].offs = dyn->native_size; EMIT(ARCH_NOP);}      \
+        } while(0)
 #define CALLRET_LOOP()   do { dyn->callrets[dyn->callret_size].type = 1; dyn->callrets[dyn->callret_size++].offs = dyn->native_size; EMIT(ARCH_NOP); } while(0)

src/dynarec/arm64/dynarec_arm64_pass3.h
@@ -42,5 +42,21 @@
                 MESSAGE(LOG_DUMP, "  Table64C: 0x%lx\n", (V)); LDRx_literal(A, val64offset);\
             } while(0)
 #define FTABLE64(A, V)  do {mmx87_regs_t v = {.d = V}; int val64offset = Table64(dyn, v.q, 3); MESSAGE(LOG_DUMP, "  FTable64: %g\n", v.d); VLDR64_literal(A, val64offset);} while(0)
-#define CALLRET_RET()   do { if(BOX64DRENV(dynarec_callret)>1) {dyn->callrets[dyn->callret_size].type = 0; dyn->callrets[dyn->callret_size++].offs = dyn->native_size; EMIT(ARCH_NOP);} } while(0)
+#define CALLRET_RET(A)                                                          \
+    do {                                                                        \
+        if((A) && BOX64DRENV(dynarec_callret)) {                                \
+            MESSAGE(LOG_DUMP, "   Dynablock*\n");                               \
+            dyn->block += sizeof(void*);                                        \
+            dyn->native_size+=sizeof(void*);                                    \
+            dyn->insts[ninst].size2 += sizeof(void*);                           \
+            dyn->sep[dyn->sep_size].x64_offs = addr - dyn->start;               \
+            dyn->sep[dyn->sep_size].nat_offs =  dyn->native_size;               \
+            ++dyn->sep_size;                                                    \
+        }                                                                       \
+        if(BOX64DRENV(dynarec_callret)>1) {                                     \
+            dyn->callrets[dyn->callret_size].type = 0;                          \
+            dyn->callrets[dyn->callret_size++].offs = dyn->native_size;         \
+            EMIT(ARCH_NOP);                                                     \
+        }                                                                       \
+    } while(0)
 #define CALLRET_LOOP()   do {dyn->callrets[dyn->callret_size].type = 1; dyn->callrets[dyn->callret_size++].offs = dyn->native_size; EMIT(ARCH_NOP); } while(0)

src/dynarec/arm64/dynarec_arm64_private.h
@@ -49,6 +49,8 @@ typedef union sse_cache_s {
     };
 } sse_cache_t;
 typedef struct callret_s callret_t;
+typedef struct sep_s sep_t;
+
 typedef struct neoncache_s {
     // Neon cache
     neon_cache_t        neoncache[32];
@@ -178,7 +180,9 @@ typedef struct dynarec_arm_s {
     instsize_t*         instsize;
     size_t              insts_size; // size of the instruction size array (calculated)
     int                 callret_size;   // size of the array
-    callret_t*          callrets;   // arrey of callret return, with NOP / UDF depending if the block is clean or dirty
+    int                 sep_size;   // size of the array
+    callret_t*          callrets;   // array of callret return, with NOP / UDF depending if the block is clean or dirty
+    sep_t*              sep;        // array of secondary entry point
     uintptr_t           forward;    // address of the last end of code while testing forward
     uintptr_t           forward_to; // address of the next jump to (to check if everything is ok)
     int32_t             forward_size;   // size at the forward point

src/dynarec/dynablock.c
@@ -46,6 +46,9 @@ dynablock_t* InvalidDynablock(dynablock_t* db, int need_lock)
         dynarec_log(LOG_DEBUG, "InvalidDynablock(%p), db->block=%p x64=%p:%p already gone=%d\n", db, db->block, db->x64_addr, db->x64_addr+db->x64_size-1, db->gone);
         // remove jumptable without waiting
         setJumpTableDefault64(db->x64_addr);
+        for(int i=0; i<db->sep_size; ++i)
+            if(db->sep[i].active)
+                setJumpTableDefault64(db->x64_addr+db->sep[i].x64_offs);
         if(need_lock)
             mutex_lock(&my_context->mutex_dyndump);
         db->done = 0;
@@ -131,8 +134,12 @@ void FreeDynablock(dynablock_t* db, int need_lock, int need_remove)
             return; // already in the process of deletion!
         dynarec_log(LOG_DEBUG, "FreeDynablock(%p), db->block=%p x64=%p:%p already gone=%d\n", db, db->block, db->x64_addr, db->x64_addr+db->x64_size-1, db->gone);
         // remove jumptable without waiting
-        if(need_remove)
+        if(need_remove) {
             setJumpTableDefault64(db->x64_addr);
+            for(int i=0; i<db->sep_size; ++i)
+                if(db->sep[i].active)
+                    setJumpTableDefault64(db->x64_addr+db->sep[i].x64_offs);
+        }
         if(need_lock)
             mutex_lock(&my_context->mutex_dyndump);
         dynarec_log(LOG_DEBUG, " -- FreeDyrecMap(%p, %d)\n", db->actual_block, db->size);
@@ -160,6 +167,9 @@ void MarkDynablock(dynablock_t* db)
 {
     if(db) {
         dynarec_log(LOG_DEBUG, "MarkDynablock %p %p-%p\n", db, db->x64_addr, db->x64_addr+db->x64_size-1);
+        for(int i=0; i<db->sep_size; ++i)
+            if(db->sep[i].active)
+                setJumpTableIfRef64(db->x64_addr+db->sep[i].x64_offs, db->jmpnext, db->block+db->sep[i].nat_offs);
         if(!setJumpTableIfRef64(db->x64_addr, db->jmpnext, db->block)) {
             dynablock_t* old = db;
             db = getDB((uintptr_t)old->x64_addr);
@@ -173,14 +183,15 @@ void MarkDynablock(dynablock_t* db)
                 else
                     db->previous = old;
             }
-        } 
         #ifdef ARCH_NOP
-        else if(db->callret_size) {
-            // mark all callrets to UDF
-            for(int i=0; i<db->callret_size; ++i)
-                *(uint32_t*)(db->block+db->callrets[i].offs) = ARCH_UDF;
-        }
+        } else {
+            if(db->callret_size) {
+                // mark all callrets to UDF
+                for(int i=0; i<db->callret_size; ++i)
+                    *(uint32_t*)(db->block+db->callrets[i].offs) = ARCH_UDF;
+            }
         #endif
+        }
     }
 }
 
@@ -318,6 +329,14 @@ static dynablock_t* internalDBGetBlock(x64emu_t* emu, uintptr_t addr, uintptr_t 
                 block->done = 1;    // don't validate the block if the size is null, but keep the block
                 rb_inc(my_context->db_sizes, block->x64_size, block->x64_size+1);
             }
+            for(int i=0; i<block->sep_size; ++i) {
+                uint32_t x64_offs = block->sep[i].x64_offs;
+                uint32_t nat_offs = block->sep[i].nat_offs;
+                if(addJumpTableIfDefault64(block->x64_addr+x64_offs, (block->dirty || block->always_test)?block->jmpnext:(block->block+nat_offs)))
+                    block->sep[i].active = 1;
+                else
+                    block->sep[i].active = 0;
+            }
         }
     }
     if(need_lock)
@@ -348,6 +367,15 @@ dynablock_t* DBGetBlock(x64emu_t* emu, uintptr_t addr, int create, int is32bits)
                         FreeDynablock(db, 0, 0);
                         db = getDB(addr);
                         MarkDynablock(db);   // just in case...
+                    } else {
+                        for(int i=0; i<db->sep_size; ++i) {
+                            uint32_t x64_offs = db->sep[i].x64_offs;
+                            uint32_t nat_offs = db->sep[i].nat_offs;
+                            if(addJumpTableIfDefault64(db->x64_addr+x64_offs, (db->always_test)?db->jmpnext:(db->block+nat_offs)))
+                                db->sep[i].active = 1;
+                            else
+                                db->sep[i].active = 0;
+                        }
                     }
                     mutex_unlock(&my_context->mutex_dyndump);
                     return db;
@@ -388,6 +416,14 @@ dynablock_t* DBGetBlock(x64emu_t* emu, uintptr_t addr, int create, int is32bits)
                     }
                     #endif
                     protectDBJumpTable((uintptr_t)db->x64_addr, db->x64_size, db->block, db->jmpnext);
+                    for(int i=0; i<db->sep_size; ++i) {
+                        uint32_t x64_offs = db->sep[i].x64_offs;
+                        uint32_t nat_offs = db->sep[i].nat_offs;
+                        if(addJumpTableIfDefault64(db->x64_addr+x64_offs, (db->always_test)?db->jmpnext:(db->block+nat_offs)))
+                            db->sep[i].active = 1;
+                        else
+                            db->sep[i].active = 0;
+                    }
                 }
             }
         }
@@ -433,6 +469,14 @@ dynablock_t* DBAlternateBlock(x64emu_t* emu, uintptr_t addr, uintptr_t filladdr,
                 }
                 #endif
                 protectDBJumpTable((uintptr_t)db->x64_addr, db->x64_size, db->block, db->jmpnext);
+                for(int i=0; i<db->sep_size; ++i) {
+                    uint32_t x64_offs = db->sep[i].x64_offs;
+                    uint32_t nat_offs = db->sep[i].nat_offs;
+                    if(addJumpTableIfDefault64(db->x64_addr+x64_offs, (db->always_test)?db->jmpnext:(db->block+nat_offs)))
+                        db->sep[i].active = 1;
+                    else
+                        db->sep[i].active = 0;
+                }
             }
         }
         mutex_unlock(&my_context->mutex_dyndump);

commit 388d9c88a4740fad14cfbb3c063ef6678b66a684

[DYNAREC] Rework SEP, fixing a speed regression from b07e2ea9d7a319df7d61ddf21e21a43b86b72b78 and introducing BOX64_DYNAREC_SEP to control them

docs/box64.pod
@@ -182,6 +182,15 @@ Optimize CALL/RET opcodes. Available in WowBox64.
  * 2 : Try to optimize CALL/RET, skipping the jump table when possible, adding code to handle return to dirty/modified block. Does not work on WowBox64. 
 
 
+=item B<BOX64_DYNAREC_SEP> =I<0|1|2>
+
+Optimize CALL/RET further with Secondary Entry Points (SEP). Have no effect is CALLRET is not enabled Available in WowBox64.
+
+ * 0 : Do not add SEP at CALLRET return 
+ * 1 : Add SEP at CALLRET return, on meory that are from known binary files [Default]
+ * 2 : Add SEP at CALLRET return for all type of memory. 
+
+
 =item B<BOX64_DYNAREC_DF> =I<0|1>
 
 Enable or disable the use of deferred flags. Available in WowBox64.

src/custommem.c
@@ -1985,9 +1985,9 @@ int addJumpTableIfDefault64(void* addr, void* jmp)
     idx0 = (((uintptr_t)addr)                )&JMPTABLE_MASK0;
 
     #ifdef JMPTABL_SHIFT4
-    return (native_lock_storeifref(create_jmptbl(0, idx0, idx1, idx2, idx3, idx4), jmp, native_next)==jmp)?1:0;
+    return (native_lock_storeifref2(create_jmptbl(0, idx0, idx1, idx2, idx3, idx4), jmp, native_next)==native_next)?1:0;
     #else
-    return (native_lock_storeifref(create_jmptbl(0, idx0, idx1, idx2, idx3), jmp, native_next)==jmp)?1:0;
+    return (native_lock_storeifref2(create_jmptbl(0, idx0, idx1, idx2, idx3), jmp, native_next)==native_next)?1:0;
     #endif
 }
 void setJumpTableDefault64(void* addr)
@@ -2033,9 +2033,9 @@ int setJumpTableDefaultIfRef64(void* addr, void* jmp)
         return 0;
     idx0 = (((uintptr_t)addr)    )&JMPTABLE_MASK0;
     #ifdef JMPTABL_SHIFT4
-    return (native_lock_storeifref(create_jmptbl(0, idx0, idx1, idx2, idx3, idx4), native_next, jmp)==native_next)?1:0;
+    return (native_lock_storeifref2(create_jmptbl(0, idx0, idx1, idx2, idx3, idx4), native_next, jmp)==jmp)?1:0;
     #else
-    return (native_lock_storeifref(create_jmptbl(0, idx0, idx1, idx2, idx3), native_next, jmp)==native_next)?1:0;
+    return (native_lock_storeifref2(create_jmptbl(0, idx0, idx1, idx2, idx3), native_next, jmp)==jmp)?1:0;
     #endif
 }
 void setJumpTableDefaultRef64(void* addr, void* jmp)
@@ -2058,7 +2058,7 @@ void setJumpTableDefaultRef64(void* addr, void* jmp)
     if(box64_jmptbl3[idx3][idx2][idx1] == box64_jmptbldefault0)
         return;
     idx0 = (((uintptr_t)addr)    )&JMPTABLE_MASK0;
-    native_lock_storeifref(&box64_jmptbl3[idx3][idx2][idx1][idx0], native_next, jmp);
+    native_lock_storeifref2(&box64_jmptbl3[idx3][idx2][idx1][idx0], native_next, jmp);
 }
 int setJumpTableIfRef64(void* addr, void* jmp, void* ref)
 {
@@ -2071,9 +2071,9 @@ int setJumpTableIfRef64(void* addr, void* jmp, void* ref)
     idx1 = (((uintptr_t)addr)>>JMPTABL_START1)&JMPTABLE_MASK1;
     idx0 = (((uintptr_t)addr)    )&JMPTABLE_MASK0;
     #ifdef JMPTABL_SHIFT4
-    return (native_lock_storeifref(create_jmptbl(0, idx0, idx1, idx2, idx3, idx4), jmp, ref)==jmp)?1:0;
+    return (native_lock_storeifref2(create_jmptbl(0, idx0, idx1, idx2, idx3, idx4), jmp, ref)==ref)?1:0;
     #else
-    return (native_lock_storeifref(create_jmptbl(0, idx0, idx1, idx2, idx3), jmp, ref)==jmp)?1:0;
+    return (native_lock_storeifref2(create_jmptbl(0, idx0, idx1, idx2, idx3), jmp, ref)==ref)?1:0;
     #endif
 }
 int isJumpTableDefault64(void* addr)
@@ -2139,7 +2139,7 @@ uintptr_t getJumpTableAddress64(uintptr_t addr)
     #endif
 }
 
-dynablock_t* getDB(uintptr_t addr)
+dynablock_t* getDBBlock(uintptr_t addr, void** jblock)
 {
     uintptr_t idx3, idx2, idx1, idx0;
     #ifdef JMPTABL_SHIFT4
@@ -2154,27 +2154,23 @@ dynablock_t* getDB(uintptr_t addr)
     #else
     uintptr_t ret = (uintptr_t)box64_jmptbl3[idx3][idx2][idx1][idx0];
     #endif
+    if(jblock) *jblock = (void*)ret;
 
     return *(dynablock_t**)(ret - sizeof(void*));
 }
 
+dynablock_t* getDB(uintptr_t addr)
+{
+    return getDBBlock(addr, NULL);
+}
+
 int getNeedTest(uintptr_t addr)
 {
-    uintptr_t idx3, idx2, idx1, idx0;
-    #ifdef JMPTABL_SHIFT4
-    uintptr_t idx4 = (((uintptr_t)addr)>>JMPTABL_START4)&JMPTABLE_MASK4;
-    #endif
-    idx3 = ((addr)>>JMPTABL_START3)&JMPTABLE_MASK3;
-    idx2 = ((addr)>>JMPTABL_START2)&JMPTABLE_MASK2;
-    idx1 = ((addr)>>JMPTABL_START1)&JMPTABLE_MASK1;
-    idx0 = ((addr)                )&JMPTABLE_MASK0;
-    #ifdef JMPTABL_SHIFT4
-    uintptr_t ret = (uintptr_t)box64_jmptbl4[idx4][idx3][idx2][idx1][idx0];
-    #else
-    uintptr_t ret = (uintptr_t)box64_jmptbl3[idx3][idx2][idx1][idx0];
-    #endif
-    dynablock_t* db = *(dynablock_t**)(ret - sizeof(void*));
-    return db?((ret!=(uintptr_t)db->block)?1:0):0;
+    void* jblock = NULL;
+    dynablock_t* db = getDBBlock(addr, &jblock);
+    if(!db) return 0;
+    if(jblock==db->jmpnext) return 1;
+    return 0;
 }
 
 uintptr_t getJumpAddress64(uintptr_t addr)

src/dynarec/arm64/arm64_lock.S
@@ -261,6 +261,7 @@ arm64_atomic_storeifref:
     mov     x3, x2
     casal   x2, x1, [x0]
     cmp     x2, x3
+    bne     2f
     mov     x0, x1
     ret
 2:
@@ -291,6 +292,7 @@ arm64_atomic_storeifref_d:
     // address is x0, value is w1, w1 store to x0 only if [x0] is w2. return new [x0] value (so w1 or old value)
     mov     w3, w2
     casal   w2, w1, [x0]
+    bne     2f
     cmp     w2, w3
     mov     w0, w1
     ret

src/dynarec/arm64/dynarec_arm64_pass2.h
@@ -31,14 +31,8 @@
                 Table64(dyn, getConst(V), 2); EMIT(0);                          \
         } while(0)
 #define FTABLE64(A, V)  do { mmx87_regs_t v = {.d = V}; Table64(dyn, v.q, 2); EMIT(0); } while(0)
-#ifdef WIN32
 #define CALLRET_RET(A)   do {                                                                                                                                                   \
+                if((A) && ISSEP() && BOX64DRENV(dynarec_callret)) { dyn->insts[ninst].size+=sizeof(void*); dyn->native_size+=sizeof(void*); ++dyn->sep_size; }                  \
                 if(BOX64DRENV(dynarec_callret)>1) {dyn->callrets[dyn->callret_size].type = 0; dyn->callrets[dyn->callret_size++].offs = dyn->native_size; EMIT(ARCH_NOP);}      \
         } while(0)
-#else
-#define CALLRET_RET(A)   do {                                                                                                                                                   \
-                if((A) && BOX64DRENV(dynarec_callret)) { dyn->insts[ninst].size+=sizeof(void*); dyn->native_size+=sizeof(void*); ++dyn->sep_size; }                             \
-                if(BOX64DRENV(dynarec_callret)>1) {dyn->callrets[dyn->callret_size].type = 0; dyn->callrets[dyn->callret_size++].offs = dyn->native_size; EMIT(ARCH_NOP);}      \
-        } while(0)
-#endif
 #define CALLRET_LOOP()   do { dyn->callrets[dyn->callret_size].type = 1; dyn->callrets[dyn->callret_size++].offs = dyn->native_size; EMIT(ARCH_NOP); } while(0)

src/dynarec/arm64/dynarec_arm64_pass3.h
@@ -43,19 +43,9 @@
                 MESSAGE(LOG_DUMP, "  Table64C: 0x%lx\n", (V)); LDRx_literal(A, val64offset);\
             } while(0)
 #define FTABLE64(A, V)  do {mmx87_regs_t v = {.d = V}; int val64offset = Table64(dyn, v.q, 3); MESSAGE(LOG_DUMP, "  FTable64: %g\n", v.d); VLDR64_literal(A, val64offset);} while(0)
-#ifdef WIN32
 #define CALLRET_RET(A)                                                          \
     do {                                                                        \
-        if(BOX64DRENV(dynarec_callret)>1) {                                     \
-            dyn->callrets[dyn->callret_size].type = 0;                          \
-            dyn->callrets[dyn->callret_size++].offs = dyn->native_size;         \
-            EMIT(ARCH_NOP);                                                     \
-        }                                                                       \
-    } while(0)
-#else
-#define CALLRET_RET(A)                                                          \
-    do {                                                                        \
-        if((A) && BOX64DRENV(dynarec_callret)) {                                \
+        if((A) && ISSEP() && BOX64DRENV(dynarec_callret)) {                     \
             MESSAGE(LOG_DUMP, "   Dynablock*\n");                               \
             dyn->block += sizeof(void*);                                        \
             dyn->native_size+=sizeof(void*);                                    \
@@ -70,5 +60,4 @@
             EMIT(ARCH_NOP);                                                     \
         }                                                                       \
     } while(0)
-#endif
 #define CALLRET_LOOP()   do {dyn->callrets[dyn->callret_size].type = 1; dyn->callrets[dyn->callret_size++].offs = dyn->native_size; EMIT(ARCH_NOP); } while(0)

src/dynarec/arm64/dynarec_arm64_private.h
@@ -198,6 +198,7 @@ typedef struct dynarec_arm_s {
     uint8_t             use_xmm:1;
     uint8_t             use_ymm:1;
     uint8_t             have_purge:1;   // set to 1 if block can be purged
+    uint8_t             is_file_mapped:1;   // if the memory is a mapped file (probably binary, not a memory)
     void*               gdbjit_block;
     uint32_t            need_x87check;  // needs x87 precision control check if non-null, or 0 if not
     uint32_t            need_dump;     // need to dump the block

src/dynarec/dynablock.c
@@ -331,8 +331,8 @@ static dynablock_t* internalDBGetBlock(x64emu_t* emu, uintptr_t addr, uintptr_t 
                 rb_inc(my_context->db_sizes, block->x64_size, block->x64_size+1);
             }
             for(int i=0; i<block->sep_size; ++i) {
-                uint32_t x64_offs = block->sep[i].x64_offs;
-                uint32_t nat_offs = block->sep[i].nat_offs;
+                uintptr_t x64_offs = block->sep[i].x64_offs;
+                uintptr_t nat_offs = block->sep[i].nat_offs;
                 if(addJumpTableIfDefault64(block->x64_addr+x64_offs, (block->dirty || block->always_test)?block->jmpnext:(block->block+nat_offs)))
                     block->sep[i].active = 1;
                 else
@@ -420,14 +420,15 @@ dynablock_t* DBGetBlock(x64emu_t* emu, uintptr_t addr, int create, int is32bits)
                     }
                     #endif
                     protectDBJumpTable((uintptr_t)db->x64_addr, db->x64_size, db->block, db->jmpnext);
-                    for(int i=0; i<db->sep_size; ++i) {
-                        uint32_t x64_offs = db->sep[i].x64_offs;
-                        uint32_t nat_offs = db->sep[i].nat_offs;
-                        if(addJumpTableIfDefault64(db->x64_addr+x64_offs, (db->always_test)?db->jmpnext:(db->block+nat_offs)))
-                            db->sep[i].active = 1;
-                        else
-                            db->sep[i].active = 0;
-                    }
+                    if(!db->always_test)
+                        for(int i=0; i<db->sep_size; ++i) {
+                            uint32_t x64_offs = db->sep[i].x64_offs;
+                            uint32_t nat_offs = db->sep[i].nat_offs;
+                            if(setJumpTableIfRef64(db->x64_addr+x64_offs, (db->always_test)?db->jmpnext:(db->block+nat_offs), db->jmpnext))
+                                db->sep[i].active = 1;
+                            else
+                                db->sep[i].active = 0;
+                        }
                 }
             }
         }

src/dynarec/dynarec_helper.h
@@ -212,6 +212,10 @@
         READFLAGS(dyn->insts[ninst].x64.need_before&~X_PEND);                                               \
     }
 
+// Insert a Secondary Entry Point if it's asked, but only on block that are from file mapped memory (so some binary file)
+//  and that are not flagged as always_test (as this could slow down a bit, and the block is unsafe), unless SEP level 2 is asked
+#define ISSEP() (BOX64DRENV(dynarec_sep) && ((dyn->is_file_mapped && !dyn->always_test) || BOX64DRENV(dynarec_sep)>1))
+
 int is_addr_unaligned(uintptr_t addr);
 int is_addr_autosmc(uintptr_t addr);
 

src/dynarec/dynarec_native_pass.c
@@ -76,6 +76,7 @@ uintptr_t native_pass(dynarec_native_t* dyn, uintptr_t addr, int alternate, int 
     dyn->forward_size = 0;
     dyn->forward_ninst = 0;
     dyn->ymm_zero = 0;
+    dyn->is_file_mapped = IsAddrElfOrFileMapped(addr);
     int dynarec_dirty = BOX64ENV(dynarec_dirty);
     #if STEP == 0
     memset(&dyn->insts[ninst], 0, sizeof(instruction_native_t));
@@ -87,7 +88,7 @@ uintptr_t native_pass(dynarec_native_t* dyn, uintptr_t addr, int alternate, int 
     ARCH_INIT();
     int reset_n = -1; // -1 no reset; -2 reset to 0; else reset to the state of reset_n
     dyn->last_ip = (alternate || (dyn->insts && dyn->insts[0].pred_sz))?0:ip;  // RIP is always set at start of block unless there is a predecessor!
-    int stopblock = 2 + !IsAddrElfOrFileMapped(addr);                          // if block is in elf memory or file mapped memory, it can be extended with BOX64DRENV(dynarec_bigblock)==2, else it needs 3
+    int stopblock = 2 + !dyn->is_file_mapped;                          // if block is in elf memory or file mapped memory, it can be extended with BOX64DRENV(dynarec_bigblock)==2, else it needs 3
     // ok, go now
     INIT;
     #if STEP == 0

src/dynarec/la64/dynarec_la64_private.h
@@ -177,6 +177,7 @@ typedef struct dynarec_la64_s {
     uint8_t              use_mmx:1;
     uint8_t              use_xmm:1;
     uint8_t              use_ymm:1;
+    uint8_t              is_file_mapped:1;
     void*                gdbjit_block;
     uint32_t             need_x87check; // x87 low precision check
     uint32_t             need_dump;     // need to dump the block

src/dynarec/rv64/dynarec_rv64_private.h
@@ -186,6 +186,7 @@ typedef struct dynarec_rv64_s {
     uint8_t             inst_sew;       // sew inside current instruction, for vsetvli elimination
     uint8_t             inst_vl;        // vl inside current instruction, for vsetvli elimination
     uint8_t             inst_vlmul;     // vlmul inside current instruction
+    uint8_t             is_file_mapped; // if the memory is a mapped file (probably binary, not a memory)
     void*               gdbjit_block;
     uint32_t            need_x87check; // x87 low precision check
     uint32_t            need_dump;     // need to dump the block

src/include/custommem.h
@@ -49,6 +49,7 @@ void addDBFromAddressRange(uintptr_t addr, size_t size);
 int cleanDBFromAddressRange(uintptr_t addr, size_t size, int destroy);
 
 dynablock_t* getDB(uintptr_t idx);
+dynablock_t* getDBBlock(uintptr_t idx, void** jblock);
 int getNeedTest(uintptr_t idx);
 int addJumpTableIfDefault64(void* addr, void* jmp); // return 1 if write was succesfull
 int setJumpTableIfRef64(void* addr, void* jmp, void* ref); // return 1 if write was succesfull

src/include/env.h
@@ -52,6 +52,7 @@ extern char* ftrace_name;
     INTEGER(BOX64_DYNAREC_BIGBLOCK, dynarec_bigblock, 2, 0, 3, 1)             \
     BOOLEAN(BOX64_DYNAREC_BLEEDING_EDGE, dynarec_bleeding_edge, 1, 0)         \
     INTEGER(BOX64_DYNAREC_CALLRET, dynarec_callret, 0, 0, 2, 1)               \
+    INTEGER(BOX64_DYNAREC_SEP, dynarec_sep, 1, 0, 2, 1)                       \
     BOOLEAN(BOX64_DYNAREC_DF, dynarec_df, 1, 1)                               \
     INTEGER(BOX64_DYNAREC_DIRTY, dynarec_dirty, 0, 0, 2, 0)                   \
     BOOLEAN(BOX64_DYNAREC_HOTPAGE_ALT, dynarec_hotpage_alt, 1, 0)             \
@@ -242,6 +243,7 @@ void WillRemoveMapping(uintptr_t addr, size_t length);
 void RemoveMapping(uintptr_t addr, size_t length);
 box64env_t* GetCurEnvByAddr(uintptr_t addr);
 int IsAddrFileMapped(uintptr_t addr, const char** filename, uintptr_t* start);
+int IsAddrFileMappedNoMemFD(uintptr_t addr);
 size_t SizeFileMapped(uintptr_t addr);
 mmaplist_t* GetMmaplistByAddr(uintptr_t addr);
 int IsAddrNeedReloc(uintptr_t addr);

src/os/os_linux.c
@@ -157,7 +157,7 @@ void PersonalityAddrLimit32Bit(void)
 
 int IsAddrElfOrFileMapped(uintptr_t addr)
 {
-    return FindElfAddress(my_context, addr) || IsAddrFileMapped(addr, NULL, NULL);
+    return FindElfAddress(my_context, addr) || IsAddrFileMappedNoMemFD(addr);
 }
 
 void* InternalMmap(void* addr, unsigned long length, int prot, int flags, int fd, ssize_t offset)

src/tools/env.c
@@ -232,6 +232,7 @@ static void applyCustomRules()
         } else if (!strcasecmp(box64env.profile, "default")) {
         } else if (!strcasecmp(box64env.profile, "fast")) {
             SET_BOX64ENV_IF_EMPTY(dynarec_callret, 1);
+            SET_BOX64ENV_IF_EMPTY(dynarec_sep, 1);
             SET_BOX64ENV_IF_EMPTY(dynarec_bigblock, 3);
             SET_BOX64ENV_IF_EMPTY(dynarec_safeflags, 0);
             SET_BOX64ENV_IF_EMPTY(dynarec_strongmem, 1);
@@ -239,6 +240,7 @@ static void applyCustomRules()
             SET_BOX64ENV_IF_EMPTY(dynarec_forward, 1024);
         } else if (!strcasecmp(box64env.profile, "fastest")) {
             SET_BOX64ENV_IF_EMPTY(dynarec_callret, 1);
+            SET_BOX64ENV_IF_EMPTY(dynarec_sep, 2);
             SET_BOX64ENV_IF_EMPTY(dynarec_bigblock, 3);
             SET_BOX64ENV_IF_EMPTY(dynarec_safeflags, 0);
             SET_BOX64ENV_IF_EMPTY(dynarec_strongmem, 0);
@@ -1481,6 +1483,18 @@ int IsAddrFileMapped(uintptr_t addr, const char** filename, uintptr_t* start)
         return 1;
     }
     return 0;
+}
+
+int IsAddrFileMappedNoMemFD(uintptr_t addr)
+{
+    const char* filename = NULL;
+    if(!IsAddrFileMapped(addr, &filename, NULL))
+        return 0;
+    if(!filename)
+        return 0;
+    if(strstr(filename, "/memfd:")==filename)
+        return 0;
+    return 1;
 }
 
 size_t SizeFileMapped(uintptr_t addr)