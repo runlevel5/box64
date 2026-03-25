runlevel5/box64

commit 2df07b594d3cda268ea18faa746743cea5d62499

[DYNAREC] Refactored how Alt are undled with Dynarec

src/include/alternate.h
@@ -1,10 +1,17 @@
 #ifndef __ALTERNATE_H__
 #define __ALTERNATE_H__
+#include <stdint.h>
+
+#if defined(DYNAREC) && !defined(WIN32)
+#define HAVE_ALTJUMP
+#endif
 
 int hasAlternate(void* addr);
 void* getAlternate(void* addr);
 void addAlternate(void* addr, void* alt);
 void addCheckAlternate(void* addr, void* alt);
 void cleanAlternate(void);
-
+#ifdef HAVE_ALTJUMP
+uintptr_t getAlternateJump(void* addr, int is32bits);
+#endif
 #endif //__ALTERNATE_H__

src/tools/alternate.c
@@ -1,9 +1,23 @@
+#include <stdlib.h>
+#include <stddef.h>
+
 #include "alternate.h"
 #include "custommem.h"
 #include "khash.h"
+#ifdef DYNAREC
+#include "box64context.h"
+#include "bridge.h"
+#endif
+#include "debug.h"
 
 // Alternate address handling
-KHASH_MAP_INIT_INT64(alternate, void*)
+typedef struct {
+    void* addr;
+    #ifdef HAVE_ALTJUMP
+    uintptr_t jump;
+    #endif
+} my_alternate_t;
+KHASH_MAP_INIT_INT64(alternate, my_alternate_t)
 static kh_alternate_t *my_alternates = NULL;
 
 int hasAlternate(void* addr) {
@@ -20,18 +34,23 @@ void* getAlternate(void* addr) {
         return addr;
     khint_t k = kh_get(alternate, my_alternates, (uintptr_t)addr);
     if(k!=kh_end(my_alternates))
-        return kh_value(my_alternates, k);
+        return kh_value(my_alternates, k).addr;
     return addr;
 }
 void addAlternate(void* addr, void* alt) {
     if(!my_alternates) {
         my_alternates = kh_init(alternate);
     }
+    if(addr==alt)
+        return; // nothing to do, alt is same as addr, but it's certainly an issue somewere!
     int ret;
     khint_t k = kh_put(alternate, my_alternates, (uintptr_t)addr, &ret);
     if(!ret)    // already there
         return;
-    kh_value(my_alternates, k) = alt;
+    kh_value(my_alternates, k).addr = alt;
+    #ifdef HAVE_ALTJUMP
+    kh_value(my_alternates, k).jump = AddAltJump(my_context->alternates, (uintptr_t)addr, (uintptr_t)alt);
+    #endif
 }
 
 void addCheckAlternate(void* addr, void* alt) {
@@ -45,3 +64,18 @@ void cleanAlternate() {
         my_alternates = NULL;
     }
 }
+
+#ifdef HAVE_ALTJUMP
+#include "bridge_private.h"
+uintptr_t getAlternateJump(void* addr, int is32bits) {
+    if(!my_alternates)
+        return 0;
+    khint_t k = kh_get(alternate, my_alternates, (uintptr_t)addr);
+    if(k!=kh_end(my_alternates)) {
+        uintptr_t ret = kh_value(my_alternates, k).jump;
+        if(is32bits) ret += offsetof(onebridge_t, FF_2);
+        return ret;
+    }
+    return 0;
+}
+#endif

src/box64context.c
@@ -186,6 +186,12 @@ box64context_t *NewBox64Context(int argc)
     context->local_maplib = NewLibrarian(context);
     context->versym = NewDictionnary();
     context->system = NewBridge();
+    #ifdef DYNAREC
+    int old_is32bits = box64_is32bits;
+    box64_is32bits = 1;
+    context->alternates = NewBridge();  // this needs to be in 32bits address space
+    box64_is32bits = old_is32bits;
+    #endif
     // Cannot use Bridge name as the map is not initialized yet
     // create vsyscall
     context->vsyscall = AddBridge(context->system, vFEv, box64_is32bits?x86Syscall:x64Syscall, 0, NULL);
@@ -357,6 +363,9 @@ void FreeBox64Context(box64context_t** context)
     box_free(ctx->pythonpath);
 
     FreeBridge(&ctx->system);
+    #ifdef DYNAREC
+    FreeBridge(&ctx->alternates);
+    #endif
 
     #ifndef STATICBUILD
     freeGLProcWrapper(ctx);

src/custommem.c
@@ -2173,6 +2173,16 @@ int getNeedTest(uintptr_t addr)
     return 0;
 }
 
+dynablock_t* getDBnoTest(uintptr_t addr)
+{
+    void* jblock = NULL;
+    dynablock_t* db = getDBBlock(addr, &jblock);
+    if(!db) return NULL;
+    if(jblock==db->jmpnext) return NULL;
+    return db;
+}
+
+
 uintptr_t getJumpAddress64(uintptr_t addr)
 {
     uintptr_t idx3, idx2, idx1, idx0;
@@ -2726,7 +2736,7 @@ void refreshProtection(uintptr_t addr)
 
 void allocProtection(uintptr_t addr, size_t size, uint32_t prot)
 {
-    dynarec_log(LOG_DEBUG, "allocProtection %p:%p 0x%x\n", (void*)addr, (void*)(addr+size-1), prot);
+    dynarec_log(LOG_DEBUG, "allocProtection %p:%p 0x%x", (void*)addr, (void*)(addr+size-1), prot);
     size = ALIGN(size);
     addr &= ~(box64_pagesize-1);
     LOCK_PROT();
@@ -2734,8 +2744,11 @@ void allocProtection(uintptr_t addr, size_t size, uint32_t prot)
     uintptr_t endb; 
     int there = rb_get_end(mapallmem, addr, &val, &endb);
     // block is here or absent, no half-block handled..
-    if(!there)
+    if(!there) {
+        dynarec_log_prefix(0, LOG_DEBUG, " added\n");
         rb_set(mapallmem, addr, addr+size, MEM_EXTERNAL);
+    } else
+        dynarec_log_prefix(0, LOG_DEBUG, " ignored\n");
     UNLOCK_PROT();
     // don't need to add precise tracking probably
 }

src/dynarec/dynablock.c
@@ -237,16 +237,14 @@ void dynablock_leave_runtime(dynablock_t* db)
     return NULL if block is not found / cannot be created. 
     Don't create if create==0
 */
-static dynablock_t* internalDBGetBlock(x64emu_t* emu, uintptr_t addr, uintptr_t filladdr, int create, int need_lock, int is32bits, int is_new)
+static dynablock_t* internalDBGetBlock(x64emu_t* emu, uintptr_t addr, int create, int need_lock, int is32bits, int is_new)
 {
-    if (hasAlternate((void*)filladdr))
-        return NULL;
     const uint32_t req_prot = (box64_pagesize==4096)?(PROT_EXEC|PROT_READ):PROT_READ;
     if(BOX64ENV(nodynarec_delay) && (addr>=BOX64ENV(nodynarec_start)) && (addr<BOX64ENV(nodynarec_end)))
         return NULL;
     dynablock_t* block = getDB(addr);
     if(block || !create) {
-        if(block && getNeedTest(addr) && (getProtection(addr)&req_prot)!=req_prot)
+        if(block && getNeedTest(addr) && (getProtection(block->x64_readaddr)&req_prot)!=req_prot)
             block = NULL;
         return block;
     }
@@ -279,29 +277,21 @@ static dynablock_t* internalDBGetBlock(x64emu_t* emu, uintptr_t addr, uintptr_t 
         }
         block = getDB(addr);    // just in case
         if(block) {
-            if(block && getNeedTest(addr) && (getProtection_fast(addr)&req_prot)!=req_prot)
+            if(block && getNeedTest(addr) && (getProtection_fast(block->x64_readaddr)&req_prot)!=req_prot)
                 block = NULL;
             mutex_unlock(&my_context->mutex_dyndump);
             pthread_sigmask(SIG_SETMASK, &old_sig, NULL);
             return block;
         }
     }
-#ifndef _WIN32
-    if((getProtection_fast(filladdr)&req_prot)!=req_prot) {// cannot be run, get out of the Dynarec
-        if(need_lock)
-            mutex_unlock(&my_context->mutex_dyndump);
-        pthread_sigmask(SIG_SETMASK, &old_sig, NULL);
-        return NULL;
-    }
-#endif
-    block = FillBlock64(filladdr, (addr==filladdr)?0:1, is32bits, MAX_INSTS, is_new);
+    block = FillBlock64(addr, is32bits, MAX_INSTS, is_new);
     if(!block) {
         dynarec_log(LOG_DEBUG, "Fillblock of block %p for %p returned an error\n", block, (void*)addr);
     }
     // check size
     if(block) {
         // fill-in jumptable
-        if(!addJumpTableIfDefault64(block->x64_addr, (block->dirty || block->always_test)?block->jmpnext:block->block)) {
+        if(!addJumpTableIfDefault64((void*)addr, (block->dirty || block->always_test)?block->jmpnext:block->block)) {
             FreeDynablock(block, 0, 0);
             block = getDB(addr);
             MarkDynablock(block);   // just in case...
@@ -349,7 +339,7 @@ dynablock_t* DBSwapInvalid(x64emu_t* emu, dynablock_t* db, uintptr_t addr, int i
         old->previous = NULL;
     }
     // start again... (will create a new block)
-    db = internalDBGetBlock(emu, addr, addr, 1, 0, is32bits, 0);
+    db = internalDBGetBlock(emu, addr, 1, 0, is32bits, 0);
     if(db) {
         if(db->previous)
             FreeInvalidDynablock(db->previous, 0);
@@ -387,15 +377,15 @@ dynablock_t* DBGetBlock(x64emu_t* emu, uintptr_t addr, int create, int is32bits)
     int is_inhotpage = isInHotPage(addr);
     if(is_inhotpage && !BOX64ENV(dynarec_dirty))
         return NULL;
-    dynablock_t *db = internalDBGetBlock(emu, addr, addr, create, 1, is32bits, 1);
+    dynablock_t *db = internalDBGetBlock(emu, addr, create, 1, is32bits, 1);
     if(db && db->done && db->block && getNeedTest(addr)) {
         //if (db->always_test) SchedYield(); // just calm down...
-        uint32_t hash = X31_hash_code(db->x64_addr, db->x64_size);
+        uint32_t hash = X31_hash_code((void*)db->x64_readaddr, db->x64_size);
         mutex_lock(&my_context->mutex_dyndump)?1:0;
         if(hash!=db->hash) {
             if(is_inhotpage && db->previous) {
                 // check alternate
-                if(db->previous && !db->dirty && X31_hash_code(db->previous->x64_addr, db->previous->x64_size)==db->previous->hash) {
+                if(db->previous && !db->dirty && X31_hash_code((void*)db->previous->x64_readaddr, db->previous->x64_size)==db->previous->hash) {
                     db = DBSwitchPrevious(emu, db, addr, 0);
                     mutex_unlock(&my_context->mutex_dyndump);
                     return db;
@@ -437,47 +427,6 @@ dynablock_t* DBGetBlock(x64emu_t* emu, uintptr_t addr, int create, int is32bits)
                             else
                                 db->sep[i].active = 0;
                         }
-                }
-            }
-        }
-        mutex_unlock(&my_context->mutex_dyndump);
-    } 
-    if(!db || !db->block || !db->done)
-        emu->test.test = 0;
-    return db;
-}
-
-dynablock_t* DBAlternateBlock(x64emu_t* emu, uintptr_t addr, uintptr_t filladdr, int is32bits)
-{
-    dynarec_log(LOG_DEBUG, "Creating AlternateBlock at %p for %p%s\n", (void*)addr, (void*)filladdr, is32bits?" 32bits":"");
-    int create = 1;
-    dynablock_t *db = internalDBGetBlock(emu, addr, filladdr, create, 1, is32bits, 1);
-    if(db && db->done && db->block && (db->dirty || getNeedTest(filladdr))) {
-        if (db->always_test) SchedYield(); // just calm down...
-        mutex_lock(&my_context->mutex_dyndump);
-        uint32_t hash = X31_hash_code(db->x64_addr, db->x64_size);
-        if(hash!=db->hash) {
-            db = DBSwapInvalid(emu, db, addr, is32bits, 0);
-        } else {
-            if(db->always_test)
-                protectDB((uintptr_t)db->x64_addr, db->x64_size);
-            else {
-                #ifdef ARCH_NOP
-                if(db->callret_size) {
-                    // mark all callrets to UDF
-                    for(int i=0; i<db->callret_size; ++i)
-                        *(uint32_t*)(db->block+db->callrets[i].offs) = ARCH_NOP;
-                    ClearCache(db->block, db->size);
-                }
-                #endif
-                protectDBJumpTable((uintptr_t)db->x64_addr, db->x64_size, db->block, db->jmpnext);
-                for(int i=0; i<db->sep_size; ++i) {
-                    uint32_t x64_offs = db->sep[i].x64_offs;
-                    uint32_t nat_offs = db->sep[i].nat_offs;
-                    if(addJumpTableIfDefault64(db->x64_addr+x64_offs, (db->always_test)?db->jmpnext:(db->block+nat_offs)))
-                        db->sep[i].active = 1;
-                    else
-                        db->sep[i].active = 0;
                 }
             }
         }

src/dynarec/dynablock_private.h
@@ -26,6 +26,7 @@ typedef struct dynablock_s {
     void*           x64_addr;
     uintptr_t       x64_size;
     size_t          native_size;
+    uintptr_t       x64_readaddr;   // in case an alternate address is in effect, it can be different than x64_addr
     uint32_t        prefixsize; // size in byte of the prefix of the block
     int             size;
     uint32_t        hash;

src/dynarec/dynarec.c
@@ -33,38 +33,34 @@ void* LinkNext(x64emu_t* emu, uintptr_t addr, void* x2, uintptr_t* x3)
 {
     int is32bits = (R_CS == 0x23);
     if(!running32bits && is32bits) running32bits=1;
-    #ifdef HAVE_TRACE
+    #ifndef HAVE_ALTJUMP
+    // inefficient way to handle alternate without ALTJUMP
     uintptr_t new_addr = (uintptr_t)getAlternate((void*)addr);
+    if(new_addr!=addr) {
+        *x3 = new_addr;
+        addr = new_addr;
+    }
+    #endif
+    #ifdef HAVE_TRACE
     if(!addr) {
         dynablock_t* db = FindDynablockFromNativeAddress(x2-4);
         printf_log(LOG_INFO, "Warning, jumping to NULL address from %p (db=%p, x64addr=%p/%s)\n", x2-4, db, db?(void*)getX64Address(db, (uintptr_t)x2-4):NULL, db?getAddrFunctionName(getX64Address(db, (uintptr_t)x2-4)):"(nil)");
-    } else if(new_addr<0x10000) {
+    } else if(addr<0x10000) {
         dynablock_t* db = FindDynablockFromNativeAddress(x2-4);
-        printf_log(LOG_INFO, "Warning, jumping to low address %p->%p from %p (db=%p, x64addr=%p/%s)\n", (void*)new_addr, (void*)addr, x2-4, db, db?(void*)getX64Address(db, (uintptr_t)x2-4):NULL, db?getAddrFunctionName(getX64Address(db, (uintptr_t)x2-4)):"(nil)");
+        printf_log(LOG_INFO, "Warning, jumping to low address %p from %p (db=%p, x64addr=%p/%s)\n", (void*)addr, x2-4, db, db?(void*)getX64Address(db, (uintptr_t)x2-4):NULL, db?getAddrFunctionName(getX64Address(db, (uintptr_t)x2-4)):"(nil)");
     #ifdef BOX32
     } else if(emu->segs[_CS]==0x23 && addr>0x100000000LL) {
         dynablock_t* db = FindDynablockFromNativeAddress(x2-4);
         printf_log(LOG_INFO, "Warning, jumping to high address %p from %p (db=%p, x64addr=%p/%s)\n", (void*)addr, x2-4, db, db?(void*)getX64Address(db, (uintptr_t)x2-4):NULL, db?getAddrFunctionName(getX64Address(db, (uintptr_t)x2-4)):"(nil)");
     #endif
-    } else if(!memExist(new_addr)) {
+    } else if(!memExist(addr)) {
         dynablock_t* db = FindDynablockFromNativeAddress(x2-4);
-        printf_log(LOG_INFO, "Warning, jumping to an unmapped address %p->%p from %p (db=%p, x64addr=%p/%s)\n", (void*)new_addr, (void*)addr, x2-4, db, db?(void*)getX64Address(db, (uintptr_t)x2-4):NULL, db?getAddrFunctionName(getX64Address(db, (uintptr_t)x2-4)):"(nil)");
+        printf_log(LOG_INFO, "Warning, jumping to an unmapped address %p from %p (db=%p, x64addr=%p/%s)\n", (void*)addr, x2-4, db, db?(void*)getX64Address(db, (uintptr_t)x2-4):NULL, db?getAddrFunctionName(getX64Address(db, (uintptr_t)x2-4)):"(nil)");
     }
-    #else   //HAVE_TRACE
-    uintptr_t new_addr = addr;
     #endif
     void * jblock;
     dynablock_t* block = NULL;
-    uintptr_t old_addr = addr;
-    if(hasAlternate((void*)addr)) {
-        printf_log(LOG_DEBUG, "Jmp address has alternate: %p\n", (void*)addr);
-        addr = (uintptr_t)getAlternate((void*)addr);    // set new address
-        R_RIP = addr;   // but also new RIP!
-        *x3 = addr; // and the RIP in x27 register
-        printf_log(LOG_DEBUG, " -> %p\n", (void*)addr);
-        block = DBAlternateBlock(emu, old_addr, addr, is32bits);
-    } else
-        block = DBGetBlock(emu, addr, 1, is32bits);
+    block = DBGetBlock(emu, addr, 1, is32bits);
     if(!block) {
         #ifdef HAVE_TRACE
         if(LOG_INFO<=BOX64ENV(dynarec_log)) {
@@ -94,14 +90,14 @@ void* LinkNext(x64emu_t* emu, uintptr_t addr, void* x2, uintptr_t* x3)
         // null block, but done: go to epilog, no linker here
         return native_epilog;
     }
-    if(block->sep_size && (uintptr_t)block->x64_addr!=old_addr) {
+    if(block->sep_size && (uintptr_t)block->x64_addr!=addr) {
         jblock = NULL;
         for(int i=0; i<block->sep_size && !jblock; ++i) {
-            if(old_addr==(uintptr_t)block->x64_addr + block->sep[i].x64_offs)
+            if(addr==(uintptr_t)block->x64_addr + block->sep[i].x64_offs)
                 jblock = block->block + block->sep[i].nat_offs;
         }
         if(!jblock) {
-            printf_log(LOG_NONE, "Warning, cannot find Secondary Entry Point %p in dynablock %p\n", new_addr, block);
+            printf_log(LOG_NONE, "Warning, cannot find Secondary Entry Point %p in dynablock %p\n", addr, block);
             return native_epilog;
         }
     }
@@ -158,6 +154,14 @@ void DynaCall(x64emu_t* emu, uintptr_t addr)
     }
 }
 
+static dynablock_t* fastDBGetBlock(x64emu_t* emu, uintptr_t addr, int create, int is32bits)
+{
+    dynablock_t* ret = getDBnoTest(R_RIP);
+    if(!ret)
+        ret = DBGetBlock(emu, R_RIP, 1, is32bits);
+    return ret;
+}
+
 void EmuRun(x64emu_t* emu, int use_dynarec)
 {
     // prepare setjump for signal handling
@@ -195,7 +199,7 @@ void EmuRun(x64emu_t* emu, int use_dynarec)
         }
         if(emu->flags.need_jmpbuf)
             emu->flags.need_jmpbuf = 0;
-
+        R_RIP = (uintptr_t)getAlternate((void*)R_RIP);
 #ifdef DYNAREC
         if(!BOX64ENV(dynarec) || !use_dynarec)
 #endif
@@ -218,7 +222,7 @@ void EmuRun(x64emu_t* emu, int use_dynarec)
                     running32bits = 1;
                 }
             }
-            dynablock_t* block = (skip || ACCESS_FLAG(F_TF))?NULL:DBGetBlock(emu, R_RIP, 1, is32bits);
+            dynablock_t* block = (skip || ACCESS_FLAG(F_TF))?NULL:fastDBGetBlock(emu, R_RIP, 1, is32bits);
             if(!block || !block->block || !block->done || ACCESS_FLAG(F_TF)) {
                 skip = 0;
                 // no block, of block doesn't have DynaRec content (yet, temp is not null)

src/dynarec/dynarec_native.c
@@ -18,6 +18,7 @@
 #include "x64trace.h"
 #include "dynablock.h"
 #include "dynablock_private.h"
+#include "alternate.h"
 
 #include "dynarec_native.h"
 #include "dynarec_arch.h"
@@ -402,6 +403,7 @@ dynablock_t* CreateEmptyBlock(uintptr_t addr, int is32bits, int is_new) {
     memset(block, 0, sizeof(dynablock_t));
     // fill the block
     block->x64_addr = (void*)addr;
+    block->x64_readaddr = addr;
     block->isize = 0;
     block->done = 0;
     block->size = sz;
@@ -417,7 +419,7 @@ dynablock_t* CreateEmptyBlock(uintptr_t addr, int is32bits, int is_new) {
     return block;
 }
 
-dynablock_t* FillBlock64(uintptr_t addr, int alternate, int is32bits, int inst_max, int is_new) {
+dynablock_t* FillBlock64(uintptr_t addr, int is32bits, int inst_max, int is_new) {
     /*
         A Block must have this layout:
 
@@ -432,15 +434,32 @@ dynablock_t* FillBlock64(uintptr_t addr, int alternate, int is32bits, int inst_m
         C ..    C+sz    : arch: arch specific info (likes flags info) per inst (can be absent)
 
     */
+    const uint32_t req_prot = (box64_pagesize==4096)?(PROT_EXEC|PROT_READ):PROT_READ;
+    uintptr_t old_addr = addr;
+    #ifdef HAVE_ALTJUMP
+    uintptr_t altjump = getAlternateJump((void*)addr, is32bits);
+    if(altjump) {
+        dynarec_log(LOG_INFO, "Building a Dynablock for %p with an alternate content at %p\n", (void*)addr, (void*)altjump);
+        addr = altjump;
+    }
+    #else
+    uintptr_t altjump = 0;
+    #endif 
     if(addr>=BOX64ENV(nodynarec_start) && addr<BOX64ENV(nodynarec_end)) {
         dynarec_log(LOG_INFO, "Create empty block in no-dynarec zone\n");
-        return BOX64ENV(nodynarec_delay)?NULL:CreateEmptyBlock(addr, is32bits, is_new);
+        return BOX64ENV(nodynarec_delay)?NULL:CreateEmptyBlock(old_addr, is32bits, is_new);
     }
     int is_inhotpage = checkInHotPage(addr);
     if(is_inhotpage && !BOX64ENV(dynarec_dirty)) {
         dynarec_log(LOG_DEBUG, "Not creating dynablock at %p as in a HotPage\n", (void*)addr);
         return NULL;
     }
+#ifndef _WIN32
+    if((getProtection_fast(addr)&req_prot)!=req_prot) {// cannot be run, get out of the Dynarec
+        dynarec_log(LOG_DEBUG, "Not creating dynablock at %p because EXEC protection is missing\n", (void*)addr);
+        return NULL;
+    }
+#endif
     if(current_helper) {
         if(current_helper==redundant_helper) {
             dynarec_log(LOG_INFO, "%04d|Warning: previous FillBlock did not cleaned up correctly (helper=%p, x64addr=%p, db=%p)\n", GetTID(), current_helper, (void*)((dynarec_native_t*)current_helper)->start, ((dynarec_native_t*)current_helper)->dynablock);
@@ -525,10 +544,10 @@ dynablock_t* FillBlock64(uintptr_t addr, int alternate, int is32bits, int inst_m
             return NULL;
         case BUILD_ABORT_EMPTY:
             CancelBlock64(0);
-            return CreateEmptyBlock(addr, is32bits, is_new);
+            return CreateEmptyBlock(old_addr, is32bits, is_new);
         case BUILD_PASS0:
             // pass 0, addresses, x64 jump addresses, overall size of the block
-            end = native_pass0(&helper, addr, alternate, is32bits, inst_max);
+            end = native_pass0(&helper, addr, altjump?1:0, is32bits, inst_max);
             if(helper.abort) {
                 if(helper.size<2) {
                     if(dyn->need_dump || BOX64ENV(dynarec_log))dynarec_log(LOG_NONE, "Abort dynablock on pass0\n");
@@ -660,7 +679,7 @@ dynablock_t* FillBlock64(uintptr_t addr, int alternate, int is32bits, int inst_m
                 // NULL block after removing dead code, how is that possible?
                 dynarec_log(LOG_INFO, "Warning, null-sized dynarec block after trimming dead code (%p)\n", (void*)addr);
                 CancelBlock64(0);
-                return CreateEmptyBlock(addr, is32bits, is_new);
+                return CreateEmptyBlock(old_addr, is32bits, is_new);
             }
             UPDATE_SPECIFICS(&helper);
             // check for still valid close loop
@@ -680,7 +699,7 @@ dynablock_t* FillBlock64(uintptr_t addr, int alternate, int is32bits, int inst_m
             ResetTable64(&helper);
             helper.reloc_size = 0;
             // pass 1, float optimizations, first pass for flags
-            native_pass1(&helper, addr, alternate, is32bits, inst_max);
+            native_pass1(&helper, addr, altjump?1:0, is32bits, inst_max);
             if(helper.abort) {
                 if(dyn->need_dump || BOX64ENV(dynarec_log))dynarec_log(LOG_NONE, "Abort dynablock on pass1\n");
                 state = BUILD_ABORT_NULL;
@@ -698,7 +717,7 @@ dynablock_t* FillBlock64(uintptr_t addr, int alternate, int is32bits, int inst_m
             helper.reloc_size = 0;
             // pass 2, instruction size
             helper.callrets = static_callrets;
-            native_pass2(&helper, addr, alternate, is32bits, inst_max);
+            native_pass2(&helper, addr, altjump?1:0, is32bits, inst_max);
             if(helper.abort) {
                 if(dyn->need_dump || BOX64ENV(dynarec_log))dynarec_log(LOG_NONE, "Abort dynablock on pass2\n");
                 state = BUILD_ABORT_NULL;
@@ -721,7 +740,7 @@ dynablock_t* FillBlock64(uintptr_t addr, int alternate, int is32bits, int inst_m
                 --imax;
                 if(dyn->need_dump || BOX64ENV(dynarec_log))dynarec_log(LOG_NONE, "Dynablock oversized, with %zu (max=%zd), recomputing cutting at %d from %d\n", native_size, MAXBLOCK_SIZE, imax, helper.size);
                 CancelBlock64(0);
-                return FillBlock64(addr, alternate, is32bits, imax, is_new);
+                return FillBlock64(old_addr, is32bits, imax, is_new);
             }
             insts_rsize = (helper.insts_size+2)*sizeof(instsize_t);
             insts_rsize = (insts_rsize+7)&~7;   // round the size...
@@ -734,7 +753,7 @@ dynablock_t* FillBlock64(uintptr_t addr, int alternate, int is32bits, int inst_m
             dynablock_align = (sz&7)?(8 -(sz&7)):0;    // align dynablock
             sz += dynablock_align + sizeof(dynablock_t) + reloc_size;
             //           dynablock_t*     block (arm insts)            table64               jmpnext code       instsize     arch         callrets         sep  dynablock           relocs
-            actual_p = (void*)AllocDynarecMap(addr, sz, is_new);
+            actual_p = (void*)AllocDynarecMap(old_addr, sz, is_new);
             if(actual_p==NULL) {
                 dynarec_log(LOG_INFO, "AllocDynarecMap(%p, %zu) failed, canceling block\n", (void*)addr, sz);
                 state = BUILD_ABORT_NULL;
@@ -753,6 +772,7 @@ dynablock_t* FillBlock64(uintptr_t addr, int alternate, int is32bits, int inst_m
             void* relocs = helper.need_reloc?(block+1):NULL;
             // fill the block
             block->x64_addr = (void*)addr;
+            block->x64_readaddr = addr;
             block->isize = 0;
             block->actual_block = actual_p;
             helper.relocs = relocs;
@@ -790,7 +810,7 @@ dynablock_t* FillBlock64(uintptr_t addr, int alternate, int is32bits, int inst_m
             ResetTable64(&helper); // reset table64 (but not the cap)
             helper.insts_size = 0;  // reset
             helper.reloc_size = 0;
-            native_pass3(&helper, addr, alternate, is32bits, inst_max);
+            native_pass3(&helper, addr, altjump?1:0, is32bits, inst_max);
             if(helper.abort) {
                 if(dyn->need_dump || BOX64ENV(dynarec_log))dynarec_log(LOG_NONE, "Abort dynablock on pass3\n");
                 state = BUILD_ABORT_NULL;
@@ -848,10 +868,10 @@ dynablock_t* FillBlock64(uintptr_t addr, int alternate, int is32bits, int inst_m
                 #endif
             }
             ClearCache(actual_p+sizeof(void*), native_size);   // need to clear the cache before execution...
-            block->hash = X31_hash_code(block->x64_addr, block->x64_size);
+            block->hash = X31_hash_code((void*)block->x64_readaddr, block->x64_size);
             // Check if something changed, to abort if it is
             if((helper.abort || (block->hash != hash))) {
-                dynarec_log(LOG_DEBUG, "Warning, a block changed while being processed hash(%p:%ld)=%x/%x\n", block->x64_addr, block->x64_size, block->hash, hash);
+                dynarec_log(LOG_DEBUG, "Warning, a block changed while being processed hash(%p:%ld)=%x/%x\n", block->x64_readaddr, block->x64_size, block->hash, hash);
                 state = BUILD_ABORT_NULL;
                 continue;
             }
@@ -904,6 +924,7 @@ dynablock_t* FillBlock64(uintptr_t addr, int alternate, int is32bits, int inst_m
         }
         #endif
     }
+    if(altjump) block->x64_addr = (void*)old_addr; // set the not-alt addr if a shadow jump was used
     redundant_helper = current_helper = NULL;
     //block->done = 1;
     return block;

commit ac645380fd6ec6d07a0bef65a2e1957db1332108

Fix ogg_sync_pageout hook
The commit 2df07b59 has caused regression

src/include/alternate.h
@@ -11,6 +11,8 @@ void* getAlternate(void* addr);
 void addAlternate(void* addr, void* alt);
 void addCheckAlternate(void* addr, void* alt);
 void cleanAlternate(void);
+void suppressAlternate(void* addr);
+void unsuppressAlternate(void* addr);
 #ifdef HAVE_ALTJUMP
 uintptr_t getAlternateJump(void* addr, int is32bits);
 #endif

src/mallochook.c
@@ -881,8 +881,12 @@ static void*     ogg_page_body_copy = NULL;
 
 static int my_ogg_sync_pageout_32(void* oy, void* og)
 {
-    // Call the original emulated ogg_sync_pageout
+    // Suppress the alternate for the duration of the call to avoid
+    // infinite recursion: RunFunctionFmt -> EmuRun -> getAlternate
+    // would redirect back to this hook without suppression.
+    suppressAlternate((void*)real_ogg_sync_pageout_32);
     int ret = (int)RunFunctionFmt(real_ogg_sync_pageout_32, "pp", oy, og);
+    unsuppressAlternate((void*)real_ogg_sync_pageout_32);
 
     if(ret > 0 && og) {
         // ogg_sync_pageout succeeded — og now has pointers into oy->data

src/tools/alternate.c
@@ -28,6 +28,12 @@ static kh_alternate_t* my_alternates = NULL;
 // virtually all khash lookups on the hot path (>99.9% are misses).
 static uint64_t alternate_bloom[4] = { 0, 0, 0, 0 };
 
+// Per-address suppression: when set, getAlternate/hasAlternate will
+// return "no alternate" for this specific address. Used by hooks that
+// need to call through to the original emulated function via
+// RunFunctionFmt without triggering infinite recursion.
+static uintptr_t alternate_suppress = 0;
+
 static inline void bloom_set(uintptr_t key)
 {
     // Three independent hash probes from different bit ranges of the key.
@@ -55,6 +61,8 @@ int hasAlternate(void* addr)
 {
     if (!my_alternates)
         return 0;
+    if ((uintptr_t)addr == alternate_suppress)
+        return 0;
     if (!bloom_test((uintptr_t)addr))
         return 0;
     khint_t k = kh_get(alternate, my_alternates, (uintptr_t)addr);
@@ -66,6 +74,8 @@ int hasAlternate(void* addr)
 void* getAlternate(void* addr)
 {
     if (!my_alternates)
+        return addr;
+    if ((uintptr_t)addr == alternate_suppress)
         return addr;
     khint_t k = kh_get(alternate, my_alternates, (uintptr_t)addr);
     if (k != kh_end(my_alternates))
@@ -88,6 +98,17 @@ void addAlternate(void* addr, void* alt)
     kh_value(my_alternates, k).jump = AddAltJump(my_context->alternates, (uintptr_t)addr, (uintptr_t)alt);
 #endif
     bloom_set((uintptr_t)addr);
+}
+
+void suppressAlternate(void* addr)
+{
+    alternate_suppress = (uintptr_t)addr;
+}
+
+void unsuppressAlternate(void* addr)
+{
+    (void)addr;
+    alternate_suppress = 0;
 }
 
 void addCheckAlternate(void* addr, void* alt)
@@ -113,6 +134,8 @@ void cleanAlternate()
 uintptr_t getAlternateJump(void* addr, int is32bits)
 {
     if (!my_alternates)
+        return 0;
+    if ((uintptr_t)addr == alternate_suppress)
         return 0;
     khint_t k = kh_get(alternate, my_alternates, (uintptr_t)addr);
     if (k != kh_end(my_alternates)) {